438ba1762c7125dd0f65972bf12aefd726143427
kate patch <kate.epatch@gmail.com>
2022-07-12 15:16:37 -0600

    created variables and const for buttons and highscores.

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ var countDown;
 var option;
 var question;
 var score;
+
+var b1 = document.getElementById('b1');
+var b2 = document.getElementById('b2');
+var b3 = document.getElementById('b3');
+var b4 = document.getElementById('b4');
+var submitButton = document.getElementById("submit-btn");
+
+const endHighScores = JSON.parse(localStorage.getItem("endHighScores")) || [];
+const maxHighScores = 10;
+const highScoresList = document.getElementById("high-scores");
