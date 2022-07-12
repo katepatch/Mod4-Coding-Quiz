5239e47fe0d46b8c58e212fce402e0c4531f0718
kate patch <kate.epatch@gmail.com>
2022-07-12 15:09:08 -0600

    grabbing id elements from html to the js

---
+++ script.js
@@ -0,0 +1,19 @@
+var start = document.getElementById("start");
+var quizQuestions = document.getElementById("quiz-questions");
+var questions = document.getElementById("questions");
+var answerButtons = document.getElementById("answer-buttons");
+var linkLeaderboard = document.getElementById("leader-board");
+var home = document.getElementById("back");
+var clearScores = document.getElementById("clear");
+var highScoreSubmission = document.getElementById("highscore-submit");
+var viewHighScores = document.getElementById("view-highscores");
+var initials = document.getElementById("initials");
+var timeScore = document.getElementById("time-remaining");
+var randomQuestions;
+var currentQuestion;
+
+var sec = 100;
+var countDown;
+var option;
+var question;
+var score;
